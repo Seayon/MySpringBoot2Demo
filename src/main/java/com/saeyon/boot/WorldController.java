c1b31c315e5907d4f8c01db7db502ae72b1efe2e
赵旭阳 <zhaoxuyang@wondersgroup.com>
2021-07-20 00:23:33 +0800

    自动将 properties 文件绑定到 bean 中

--- src/main/java/com/saeyon/boot/WorldController.java
+++ src/main/java/com/saeyon/boot/WorldController.java
@@ -1,5 +1,8 @@
 package com.saeyon.boot;
 
+import com.saeyon.bean.Car;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +14,11 @@ public class WorldController {
         return "World!";
     }
 
+    @Autowired
+    Car car;
+
+    @GetMapping("/car")
+    public Car car() {
+        return car;
+    }
 }
--- src/main/java/com/saeyon/config/SpringConfig.java
+++ src/main/java/com/saeyon/config/SpringConfig.java
@@ -1,10 +1,12 @@
 package com.saeyon.config;
 
 import ch.qos.logback.core.db.DBHelper;
+import com.saeyon.bean.Car;
 import com.saeyon.bean.Pet;
 import com.saeyon.bean.User;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -15,11 +17,12 @@ import org.springframework.context.annotation.ImportResource;
 //proxyBeanMethods 如果设置为 false,则为 Lite 模式,每次都会跳过检查直接调用,对这个类不进行增强
 //但是 proxyBeanMethods 设置为 false,只是不增强 下面 myUser() 这个方法,用基于类型的 引入:比如下面的 myUser(Pet pet)还是会引入进来容器中已经存在的类
 //@Import 会自动给这个类导入列表中的类型的无参构造的对象,默认的名字是全类名
-@Import({DBHelper.class,User.class})
+@Import({DBHelper.class, User.class})
 @ImportResource(value = "classpath:bean1.xml")
+@EnableConfigurationProperties(value = {Car.class})
 public class SpringConfig {
 
-//    默认返回的是方法的名字
+    //    默认返回的是方法的名字
     @Bean
     @ConditionalOnMissingBean(name = "pet", value = {Pet.class})
     public User myUser() {
@@ -28,7 +31,7 @@ public class SpringConfig {
         return zs;
     }
 
-//    @Bean
+    //    @Bean
     public Pet pet() {
         return new Pet("tomcat");
     }
