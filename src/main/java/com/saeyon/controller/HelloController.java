986de228f9f56f434172d584b2315fadb6e8bee6
赵旭阳 <zhaoxuyang@wondersgroup.com>
2021-07-23 22:27:27 +0800

    wode

--- src/main/java/com/saeyon/controller/HelloController.java
+++ src/main/java/com/saeyon/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.saeyon.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +11,11 @@ public class HelloController {
     @RequestMapping("/hello")
     public String hello01() {
         return "Hello, Spring Boot 2";
+    }
+
+    //同样情况下先去找 Controller,找不到才去找静态资源
+    @GetMapping("banner-1.jpg")
+    public String he() {
+        return "aaa";
     }
 }
